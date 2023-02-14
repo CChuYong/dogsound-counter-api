0bc110b86a5afe9c1ca4f024855538725c01ccac
ChuYong <jakgon@naver.com>
2023-02-14 22:22:50 +0900

    chore: 프로젝트 모듈구조 클린아키텍쳐화

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,5 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.springframework.boot.gradle.tasks.bundling.BootJar
 
 plugins {
 	id("org.springframework.boot") version "3.0.2"
@@ -7,35 +8,90 @@ plugins {
 	kotlin("plugin.spring") version "1.7.22"
 }
 
-group = "co.bearus"
-version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_17
-
 repositories {
 	mavenCentral()
 }
 
-dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
-	implementation("org.springframework.boot:spring-boot-starter-webflux")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
-	implementation("org.flywaydb:flyway-core")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
-	implementation("org.springframework:spring-jdbc")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("io.projectreactor:reactor-test")
+subprojects {
+	apply(plugin = "java-library")
+	apply(plugin = "org.springframework.boot")
+	apply(plugin = "io.spring.dependency-management")
+
+	group = "co.bearus"
+	version = "0.0.1-SNAPSHOT"
+	java.sourceCompatibility = JavaVersion.VERSION_17
+
+	repositories {
+		mavenCentral()
+	}
+
+	dependencies {
+		implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
+		implementation("org.springframework.boot:spring-boot-starter-webflux")
+		implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+		implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
+		implementation("org.flywaydb:flyway-core")
+		implementation("org.jetbrains.kotlin:kotlin-reflect")
+		implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+		implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+		implementation("org.springframework:spring-jdbc")
+		testImplementation("org.springframework.boot:spring-boot-starter-test")
+		testImplementation("io.projectreactor:reactor-test")
+	}
+
+	tasks.withType<KotlinCompile> {
+		kotlinOptions {
+			freeCompilerArgs = listOf("-Xjsr305=strict")
+			jvmTarget = "17"
+		}
+	}
+
+	tasks.withType<Test> {
+		useJUnitPlatform()
+	}
+
+	val jar: Jar by tasks
+	val bootJar: BootJar by tasks
+
+	bootJar.enabled = false
+	jar.enabled = false
 }
 
-tasks.withType<KotlinCompile> {
-	kotlinOptions {
-		freeCompilerArgs = listOf("-Xjsr305=strict")
-		jvmTarget = "17"
+project(":use-case") {
+	dependencies {
+		api(project(":entity"))
 	}
 }
 
-tasks.withType<Test> {
-	useJUnitPlatform()
+project(":infrastructure") {
+	dependencies {
+		api(project(":use-case"))
+		//DB
+	}
+}
+
+project(":presenter") {
+	dependencies {
+		api(project(":use-case"))
+		api("org.springframework.boot:spring-boot-starter-webflux")
+		api("org.springframework.boot:spring-boot-starter-validation")
+	}
+}
+
+project(":config") {
+	val jar: Jar by tasks
+	val bootJar: BootJar by tasks
+
+	bootJar.enabled = true
+	jar.enabled = true
+
+	dependencies {
+		api(project(":entity"))
+		api(project(":use-case"))
+		api(project(":presenter"))
+		api(project(":infrastructure"))
+
+		api("org.springframework.boot:spring-boot-starter-webflux")
+		testImplementation("org.springframework.boot:spring-boot-starter-test")
+	}
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,2 @@
 rootProject.name = "dogsound-counter-api"
+include("entity", "use-case", "infrastructure", "presenter", "config")
