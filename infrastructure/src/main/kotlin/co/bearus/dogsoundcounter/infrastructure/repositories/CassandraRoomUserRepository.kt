5c8edd88fea17072bd24345ddc7308eb1011b74e
ChuYong <jakgon@naver.com>
2023-06-17 18:21:18 +0900

    feat: allow filtering 걸기

--- infrastructure/src/main/kotlin/co/bearus/dogsoundcounter/infrastructure/repositories/CassandraRoomUserRepository.kt
+++ infrastructure/src/main/kotlin/co/bearus/dogsoundcounter/infrastructure/repositories/CassandraRoomUserRepository.kt
@@ -1,6 +1,7 @@
 package co.bearus.dogsoundcounter.infrastructure.repositories
 
 import co.bearus.dogsoundcounter.infrastructure.entities.CassandraRoomUserEntity
+import org.springframework.data.cassandra.repository.AllowFiltering
 import org.springframework.data.repository.kotlin.CoroutineCrudRepository
 import org.springframework.stereotype.Repository
 
@@ -8,5 +9,6 @@ import org.springframework.stereotype.Repository
 interface CassandraRoomUserRepository : CoroutineCrudRepository<CassandraRoomUserEntity, String> {
     suspend fun findAllByRoomId(roomId: String): List<CassandraRoomUserEntity>
     suspend fun findAllByUserId(userId: String): List<CassandraRoomUserEntity>
+    @AllowFiltering
     suspend fun findByRoomIdAndUserId(roomId: String, userId: String): CassandraRoomUserEntity
 }
