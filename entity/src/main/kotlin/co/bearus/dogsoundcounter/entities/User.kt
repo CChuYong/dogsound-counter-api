df265ce7ad3d150c1f14b0c5b9a08ec83d2a03d0
ChuYong <jakgon@naver.com>
2023-06-18 21:49:07 +0900

    feat: 기본 프로필 이미지 변경

--- entity/src/main/kotlin/co/bearus/dogsoundcounter/entities/User.kt
+++ entity/src/main/kotlin/co/bearus/dogsoundcounter/entities/User.kt
@@ -4,7 +4,7 @@ data class User(
     val userId: String,
     val email: String,
     val nickname: String,
-    val profileImgUrl: String = "https://bsc-assets.s3.ap-northeast-2.amazonaws.com/default_profile_img.webp",
+    val profileImgUrl: String = "https://bsc-assets-public.s3.ap-northeast-2.amazonaws.com/default_profile.jpeg",
     val lastSeenAtTs: Long,
     val createdAtTs: Long,
 ) {
