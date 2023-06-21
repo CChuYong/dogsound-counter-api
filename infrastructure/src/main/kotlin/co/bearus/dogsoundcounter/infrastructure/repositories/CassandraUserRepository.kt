0dafe809d6b7c6a1760a966438305131b48c6e8e
YeongMin Song <jakgon@naver.com>
2023-06-21 16:09:25 +0900

    feat: allow filtering

--- infrastructure/src/main/kotlin/co/bearus/dogsoundcounter/infrastructure/repositories/CassandraUserRepository.kt
+++ infrastructure/src/main/kotlin/co/bearus/dogsoundcounter/infrastructure/repositories/CassandraUserRepository.kt
@@ -1,12 +1,15 @@
 package co.bearus.dogsoundcounter.infrastructure.repositories
 
 import co.bearus.dogsoundcounter.infrastructure.entities.CassandraUserEntity
+import org.springframework.data.cassandra.repository.AllowFiltering
 import org.springframework.data.repository.kotlin.CoroutineCrudRepository
 import org.springframework.stereotype.Repository
 
 @Repository
 interface CassandraUserRepository : CoroutineCrudRepository<CassandraUserEntity, String> {
     suspend fun findFirstByEmail(email: String): CassandraUserEntity?
+
+    @AllowFiltering
     suspend fun findFirstByNicknameAndTag(nickname: String, tag: String): CassandraUserEntity?
     suspend fun getByUserId(userId: String): CassandraUserEntity
 }
