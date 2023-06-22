25beefb7f1eb5063839723e05d1a7cfd09cc2e83
YeongMin Song <jakgon@naver.com>
2023-06-22 18:13:21 +0900

    feat: 헬스체크 로그 제거

--- presenter/src/main/kotlin/co/bearus/dogsoundcounter/presenter/config/RequestFilter.kt
+++ presenter/src/main/kotlin/co/bearus/dogsoundcounter/presenter/config/RequestFilter.kt
@@ -13,6 +13,7 @@ class RequestFilter : WebFilter {
     private val log = LoggerFactory.getLogger(RequestFilter::class.java)
     override fun filter(exchange: ServerWebExchange, chain: WebFilterChain): Mono<Void> {
         val startTime = System.currentTimeMillis()
+        if (exchange.request.path.toString().startsWith("/actuator")) return chain.filter(exchange)
         return ReactiveSecurityContextHolder
             .getContext()
             .map { exchange.attributes["userId"] = it.authentication.name }
