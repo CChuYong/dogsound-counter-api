8f322de8f4bc7c60f4bc4288fd5ff814f4efb006
YeongMin Song <jakgon@naver.com>
2023-06-22 10:35:14 +0900

    feat: change route

--- presenter/src/main/kotlin/co/bearus/dogsoundcounter/presenter/rsockets/RSocketController.kt
+++ presenter/src/main/kotlin/co/bearus/dogsoundcounter/presenter/rsockets/RSocketController.kt
@@ -2,6 +2,7 @@ package co.bearus.dogsoundcounter.presenter.rsockets
 
 import co.bearus.dogsoundcounter.entities.ClientPacket
 import co.bearus.dogsoundcounter.usecases.message.MessageReceiver
+import co.bearus.dogsoundcounter.usecases.user.oauth.TokenProvider
 import org.springframework.messaging.handler.annotation.MessageMapping
 import org.springframework.stereotype.Controller
 import reactor.core.publisher.Flux
@@ -9,13 +10,15 @@ import reactor.core.publisher.Flux
 @Controller
 class RSocketController(
     private val messageReceiver: MessageReceiver,
+    private val tokenProvider: TokenProvider,
 ) {
-    @MessageMapping("users")
+    @MessageMapping("/v1/event-gateway")
     fun openChannel(request: RSocketSubscribeRequest): Flux<ClientPacket> {
-        return messageReceiver.createChannel(request.userId)
+        val userId = tokenProvider.extractUserIdFromToken(request.accessToken) ?: throw RuntimeException()
+        return messageReceiver.createChannel(userId)
     }
 
     data class RSocketSubscribeRequest(
-        val userId: String,
+        val accessToken: String,
     )
 }
