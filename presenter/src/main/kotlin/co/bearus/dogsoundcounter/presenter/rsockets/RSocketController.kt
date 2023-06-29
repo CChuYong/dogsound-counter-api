41f09842e5f5a441c81aed92738090324cc728cf
ChuYong <jakgon@naver.com>
2023-06-29 15:08:28 +0900

    feat: 로그 찍어보기

--- presenter/src/main/kotlin/co/bearus/dogsoundcounter/presenter/rsockets/RSocketController.kt
+++ presenter/src/main/kotlin/co/bearus/dogsoundcounter/presenter/rsockets/RSocketController.kt
@@ -3,6 +3,7 @@ package co.bearus.dogsoundcounter.presenter.rsockets
 import co.bearus.dogsoundcounter.entities.ClientPacket
 import co.bearus.dogsoundcounter.usecases.message.MessageReceiver
 import co.bearus.dogsoundcounter.usecases.user.oauth.TokenProvider
+import org.slf4j.LoggerFactory
 import org.springframework.messaging.handler.annotation.MessageMapping
 import org.springframework.stereotype.Controller
 import reactor.core.publisher.Flux
@@ -12,8 +13,10 @@ class RSocketController(
     private val messageReceiver: MessageReceiver,
     private val tokenProvider: TokenProvider,
 ) {
+    val logger = LoggerFactory.getLogger(RSocketController::class.java)
     @MessageMapping("/v1/event-gateway")
     fun openChannel(request: RSocketSubscribeRequest): Flux<ClientPacket> {
+        logger.info("Device Connected to WS Gateway!")
         val userId = tokenProvider.extractUserIdFromToken(request.accessToken) ?: throw RuntimeException()
         return messageReceiver.createChannel(userId, request.deviceToken)
     }
